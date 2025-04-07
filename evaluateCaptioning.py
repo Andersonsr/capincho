e6d96c9a4c282b368593de18817a68b9ae9c316c
Andersonsr <andersonsr2014@outlook.com>
2025-04-07 18:58:59 -0300

    petro-txt qualitative eval

--- evaluateCaptioning.py
+++ evaluateCaptioning.py
@@ -1,9 +1,10 @@
 import argparse
 import random
 import torch
-from embeddingsDataset import PetroDataset
+from embeddingsDataset import PetroDataset, COCODataset
 from decoder import model_from_json
 from tqdm import tqdm
+from textLoader import TextLoader
 
 
 if __name__ == '__main__':
@@ -15,8 +16,15 @@ if __name__ == '__main__':
                         help='split to load for evaluation')
     parser.add_argument('--random_seed', type=int, default=777, help='random seed for qualitative evaluation')
     parser.add_argument('--num_images', '-n', type=int, default=10, help='number of images to evaluate')
+    parser.add_argument('--dataset', type=str, required=True, choices=['petro', 'petro-txt', 'coco'])
+
     args = parser.parse_args()
-    data = PetroDataset(args.embeddings, split=args.split)
+    if args.dataset == 'petro':
+        data = PetroDataset(args.embeddings, split=args.split)
+    elif args.dataset == 'petro-txt':
+        data = TextLoader(args.embeddings, split=args.split, has_embeddings=True)
+    elif args.dataset == 'coco':
+        data = COCODataset(args.embeddings, n_captions=1)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model_from_json(args.model, device)
@@ -28,7 +36,14 @@ if __name__ == '__main__':
     ids = []
     for i in tqdm([random.randint(0, len(data)) for i in range(args.num_images)]):
         # print(data[i]['image_embeddings'].shape)
-        generated.append(model.caption(data[i]['image_embeddings'], max_tokens=200, )[0])
+        if args.dataset == 'petro' or args.dataset == 'coco':
+            embedding = data[i]['image_embeddings']
+        elif args.dataset == 'petro-txt':
+            embedding = data[i]['text_embeddings']
+        else:
+            raise ValueError(f'{args.dataset} is not a valid dataset')
+
+        generated.append(model.caption(embedding, max_tokens=200, )[0])
         ids.append(data[i]['image_id'])
         gt.append(data[i]['captions'])
 
